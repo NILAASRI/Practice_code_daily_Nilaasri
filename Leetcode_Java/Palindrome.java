2fa2add048ccb7dc62ef7b7084c01630b263461c
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-09-19 19:42:25 +0530

    Create Palindrome.java

---
+++ Leetcode_Java/Palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0) return false;
+        int n = x;
+        int n1;
+        int n2=0;
+        while(x!=0){
+            n1 = x % 10;
+            n2 = n2*10 + n1;
+            x = x / 10;
+        }
+        if(n==n2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
