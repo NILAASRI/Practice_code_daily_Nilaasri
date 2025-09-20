55e481c758adba647a25e1c3228b37ac607d29bb
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-09-20 23:33:45 +0530

    Create JavaString1.java

---
+++ Java/HackerrankLeetcode/JavaString1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.nextLine();
+        String B = sc.nextLine();
+
+        // 1. Sum of lengths
+        System.out.println(A.length() + B.length());
+
+        // 2. Lexicographical comparison
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        /*
+        int lexi = A.compareTo(B);
+        if(lexi<=0){
+            System.out.println("No");
+        }
+        else{
+            System.out.println("Yes");
+        }*/
+        // 3. Capitalize first letter
+        String capitalizedA = A.substring(0,1).toUpperCase() + A.substring(1);
+        String capitalizedB = B.substring(0,1).toUpperCase() + B.substring(1);
+        System.out.println(capitalizedA + " " + capitalizedB);
+    }
+}
