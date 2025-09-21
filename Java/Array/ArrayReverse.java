cf5804ae4015ed625159decd8fe313f7f353e996
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-09-21 22:41:19 +0530

    Create ArrayReverse.java

---
+++ Java/Array/ArrayReverse.java
@@ -0,0 +1,11 @@
+// Reversed Array
+import java.util.*;
+public class ArrayReverse{
+  public static void main(String[] args){
+    int [] arr = {1,2,3,4,5,6};
+    for(int i = arr.length-1; i>=0; i--){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
+//Output: 6 5 4 3 2 1 
